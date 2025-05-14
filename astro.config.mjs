f88f6b25fcc622c6d04f4e8b754cf6a4da79aa2a
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-14 12:42:21 +0530

    feat: add recursive font to the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,55 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        cssVariable: "--font-rec",
+        name: "Recursive",
+        variants: [
+          {
+            weight: 300,
+            style: "normal",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-Light.woff2"],
+          },
+          {
+            weight: 300,
+            style: "italic",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-LtItalic.woff2"],
+          },
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-Regular.woff2"],
+          },
+          {
+            weight: 400,
+            style: "italic",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-Italic.woff2"],
+          },
+          {
+            weight: 600,
+            style: "normal",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-SemiBd.woff2"],
+          },
+          {
+            weight: 600,
+            style: "italic",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-SmBdItalic.woff2"],
+          },
+          {
+            weight: 900,
+            style: "normal",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-Black.woff2"],
+          },
+          {
+            weight: 900,
+            style: "italic",
+            src: ["./src/assets/fonts/RecursiveMonoCslSt-BlkItalic.woff2"],
+          },
+        ],
+      },
+    ],
+  },
 });
